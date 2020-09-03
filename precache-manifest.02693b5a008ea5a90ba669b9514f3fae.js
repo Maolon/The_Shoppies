2f888c8ae8b5c0fbc8b9f181b38065496225880e
Maolon <gademalon@gmail.com>
2020-09-03 16:59:43 -0400

    Updates

---
+++ precache-manifest.02693b5a008ea5a90ba669b9514f3fae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827bc753b753ec40a9c56721adcff18a",
+    "url": "/The_Shoppies/index.html"
+  },
+  {
+    "revision": "1607a237e68ea3dc75bf",
+    "url": "/The_Shoppies/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "f00517cf23291cf30120",
+    "url": "/The_Shoppies/static/css/main.26db0bdb.chunk.css"
+  },
+  {
+    "revision": "1607a237e68ea3dc75bf",
+    "url": "/The_Shoppies/static/js/2.4315df5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/The_Shoppies/static/js/2.4315df5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00517cf23291cf30120",
+    "url": "/The_Shoppies/static/js/main.8dbef517.chunk.js"
+  },
+  {
+    "revision": "327e1f664aaba050500a",
+    "url": "/The_Shoppies/static/js/runtime-main.497ffe87.js"
+  },
+  {
+    "revision": "c0b2bf06a63c3db09c721bc17927707a",
+    "url": "/The_Shoppies/static/media/image.c0b2bf06.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c78da434d3829bb5a1c81821a476317f.js"
+  "/The_Shoppies/precache-manifest.02693b5a008ea5a90ba669b9514f3fae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/The_Shoppies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
