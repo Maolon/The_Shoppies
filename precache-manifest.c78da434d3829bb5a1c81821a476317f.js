48f1f6590cae92eac6e02c20e4b0356479533b33
Maolon <gademalon@gmail.com>
2020-09-03 16:56:34 -0400

    Updates

---
+++ precache-manifest.c78da434d3829bb5a1c81821a476317f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e1a1b93cc7bc6f629c52b2608ec9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1607a237e68ea3dc75bf",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "edee94b9390d16f02c19",
+    "url": "/static/css/main.26db0bdb.chunk.css"
+  },
+  {
+    "revision": "1607a237e68ea3dc75bf",
+    "url": "/static/js/2.4315df5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4315df5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edee94b9390d16f02c19",
+    "url": "/static/js/main.c4192980.chunk.js"
+  },
+  {
+    "revision": "987d794bf1933d44fcf9",
+    "url": "/static/js/runtime-main.3e8f7d28.js"
+  },
+  {
+    "revision": "c0b2bf06a63c3db09c721bc17927707a",
+    "url": "/static/media/image.c0b2bf06.svg"
+  }
+]);
